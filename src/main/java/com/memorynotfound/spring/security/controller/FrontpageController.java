676598dc4e8d36da3c333a948884eb9341fd1c77
Mikkel Dalby Nielsen <mikkel@dalbynielsen.dk>
2018-05-01 15:50:19 +0200

    no message

--- src/main/java/com/memorynotfound/spring/security/controller/FrontpageController.java
+++ src/main/java/com/memorynotfound/spring/security/controller/FrontpageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class HomeController {
+public class FrontpageController {
 
     @GetMapping("/")
     public String root() {
