07759f19d46ee224f8cde9115da19be635b3dab4
Mikkel Dalby Nielsen <mikkel@dalbynielsen.dk>
2018-05-07 15:58:29 +0200

    test

--- src/main/java/com/memorynotfound/spring/security/config/SecurityConfig.java
+++ src/main/java/com/memorynotfound/spring/security/config/SecurityConfig.java
@@ -4,14 +4,15 @@ import com.memorynotfound.spring.security.web.LoggingAccessDeniedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 import javax.sql.DataSource;
 
 @EnableWebSecurity
@@ -19,6 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private LoggingAccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/create-developer");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -29,7 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                             "/js/**",
                             "/css/**",
                             "/img/**",
-                            "/webjars/**").permitAll()
+                            "/webjars/**",
+                            "/create-developer").permitAll()
                     .antMatchers("/user/**").hasRole("USER")
                     .antMatchers("/manager/**").hasRole("manager")
                     .anyRequest().authenticated()
---
+++ src/main/java/com/memorynotfound/spring/security/model/Developer.java
@@ -0,0 +1,118 @@
+package com.memorynotfound.spring.security.model;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class Developer {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String tlf1;
+    private String tlf2;
+    private int zipCode;
+    private String city;
+    private String password;
+    private String passwordAgain;
+
+    private PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public Developer(String firstName, String lastName, String email, String tlf1, String tlf2, int zipCode, String city, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.tlf1 = tlf1;
+        this.tlf2 = tlf2;
+        this.zipCode = zipCode;
+        this.city = city;
+        this.password = encoder.encode(password);
+    }
+
+    public Developer(String firstName, String lastName, String email, String tlf1, int zipCode, String city, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.tlf1 = tlf1;
+        this.tlf2 = "";
+        this.zipCode = zipCode;
+        this.city = city;
+        this.password = encoder.encode(password);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTlf1() {
+        return tlf1;
+    }
+
+    public void setTlf1(String tlf1) {
+        this.tlf1 = tlf1;
+    }
+
+    public String getTlf2() {
+        return tlf2;
+    }
+
+    public void setTlf2(String tlf2) {
+        this.tlf2 = tlf2;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = encoder.encode(password);
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", tlf1='" + tlf1 + '\'' +
+                ", tlf2='" + tlf2 + '\'' +
+                ", zipCode=" + zipCode +
+                ", city='" + city + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
