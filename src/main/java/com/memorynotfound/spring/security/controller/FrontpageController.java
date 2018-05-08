f6c4e4135e95861113715fe8402d90d7407861e2
DESKTOP-EBQ3QD0\mega- <ludvig.andersen@yahoo.dk>
2018-05-08 13:53:44 +0200

    Ændret navn på Developer class til User

--- src/main/java/com/memorynotfound/spring/security/controller/FrontpageController.java
+++ src/main/java/com/memorynotfound/spring/security/controller/FrontpageController.java
@@ -1,6 +1,5 @@
 package com.memorynotfound.spring.security.controller;
 
-import com.memorynotfound.spring.security.model.Developer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
