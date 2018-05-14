e33c8af69cba8639a2c1a957722b77e8d607a978
Grand <christofferhansen20@gmail.com>
2018-05-14 14:28:59 +0200

    createIdea v. 0.21

--- src/main/java/com/memorynotfound/spring/security/controller/FrontpageController.java
+++ src/main/java/com/memorynotfound/spring/security/controller/FrontpageController.java
@@ -5,6 +5,8 @@ import com.memorynotfound.spring.security.model.Person;
 import com.memorynotfound.spring.security.repository.IIdeaDbRepository;
 import com.memorynotfound.spring.security.repository.IPersonDbRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,8 +47,12 @@ public class FrontpageController {
     @PostMapping("/create-idea")
     public String createIdea(
         @ModelAttribute("ideaName") String ideaName,
-        @ModelAttribute("ideaDescription") String ideaDescription,
-        @ModelAttribute("ideaPerson") int ideaPerson){
+        @ModelAttribute("ideaDescription") String ideaDescription){
+
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            String name = auth.getName();
+
+            iPersonDbRepository.getPerson(name);
 
             Idea currentIdea = new Idea(ideaName, ideaDescription, ideaPerson, LocalDate.now());
             System.out.println(currentIdea.toString());
