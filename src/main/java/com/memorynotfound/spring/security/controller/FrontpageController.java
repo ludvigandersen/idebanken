cfe480ddef983aa4fc0f2ad8016cb13b27d3f39b
Mikkel Dalby Nielsen <mikkel@dalbynielsen.dk>
2018-05-14 13:11:57 +0200

    Det compiler igen nu

--- src/main/java/com/memorynotfound/spring/security/controller/FrontpageController.java
+++ src/main/java/com/memorynotfound/spring/security/controller/FrontpageController.java
@@ -6,9 +6,12 @@ import com.memorynotfound.spring.security.repository.IIdeaDbRepository;
 import com.memorynotfound.spring.security.repository.IPersonDbRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDate;
 
 @Controller
 public class FrontpageController {
