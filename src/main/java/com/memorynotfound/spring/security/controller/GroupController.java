7d3d85724a3cb12f7aef6b3671eca549fc08646a
Hr. Nick <Nicolai@osalle4.dk>
2018-05-16 20:14:28 +0200

    Merge og Se egne grupper oprettet

--- src/main/java/com/memorynotfound/spring/security/controller/GroupController.java
+++ src/main/java/com/memorynotfound/spring/security/controller/GroupController.java
@@ -2,8 +2,12 @@ package com.memorynotfound.spring.security.controller;
 
 import com.memorynotfound.spring.security.model.Group;
 import com.memorynotfound.spring.security.repository.IGroupDbRepository;
+import com.memorynotfound.spring.security.repository.IPersonDbRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,17 +17,38 @@ public class GroupController {
 
     @Autowired
     IGroupDbRepository iGroupDbRepository;
+    @Autowired
+    IPersonDbRepository iPersonDbRepository;
 
     @GetMapping ("/create-group")
-    public String createGroup (){
+    public String createGroup (Model model){
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("PersonName",auth.getName());
         return "user/create-group";
     }
 
     @PostMapping("/create-group-post")
-    public String createGroup (@ModelAttribute("name") String groupName){
+    public String createGroup (@ModelAttribute("name") String groupName,
+                               @ModelAttribute("PersonName") String PersonName){
+
+
         Group group = new Group(groupName);
         iGroupDbRepository.createGroup(group);
+        int PersonId = iPersonDbRepository.getPersonId(PersonName);
+
+
         return "redirect:/";
+    }
+
+    @GetMapping("/groups")
+    public String developerGroups(Model model ){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName();
+        int personId = iPersonDbRepository.getPersonId(name);
+        model.addAttribute("groups", iGroupDbRepository.getDeveloperGroupsWithPersonId(personId));
+
+        return "/groups";
     }
 
 
